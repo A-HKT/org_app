93c0e1e7ad3ec4b0252b614dc7431a8991595c14
A-HKT <manekko51@yahoo.co.jp>
2022-10-16 15:08:48 +0900

    検索画面の構築

--- server/index.php
+++ server/index.php
@@ -23,7 +23,7 @@ require_once __DIR__ . '/config.php';
             </nav>
         </div>
     </header>
-    <div class="main_content wrapper">
+    <section class="main_content wrapper">
         <div class="main_title">
             <h2>
                 <span>Article</span><br>
@@ -33,30 +33,94 @@ require_once __DIR__ . '/config.php';
             <h3>見たい記事がすぐに見つかる！</h3>
         </div>
         <img class="main_image" src="images\main_image.png" alt="main">
-    </div>
+    </section>
 
-    <form>
-        <div class="dashboard wrapper">
-            <h2>記事検索</h2>
-            <div class="categories contents_form">
+    <section class="seach_form form">
+        <h2>SEACH</h2>
+        <div class="form_contents">
+            <div class="form_items">
                 <p>カテゴリ</p>
-                <input type="checkbox" name="category" value='life'>
-                <span>生活習慣病</span>
-                <input type="checkbox" name="category" value='old'>
-                <span>高齢者</span>
+                <div class="item_box">
+                    <div class="item">
+                        <input type="checkbox" name="category" value='life'>
+                        <span>生活習慣病</span>
+                    </div>
+                    <div class="item">
+                        <input type="checkbox" name="category" value='old'>
+                        <span>高齢者</span>
+                    </div>
+                    <div class="item">
+                        <input type="checkbox" name="category" value='infection'>
+                        <span>感染症</span>
+                    </div>
+                    <div class="item">
+                        <input type="checkbox" name="category" value='meal_dental'>
+                        <span>食事・口腔</span>
+                    </div>
+                    <div class="item">
+                        <input type="checkbox" name="category" value='mental'>
+                        <span>メンタル</span>
+                    </div>
+                    <div class="item">
+                        <input type="checkbox" name="category" value='work'>
+                        <span>職域</span>
+                    </div>
+                    <div class="item">
+                        <input type="checkbox" name="category" value='firstaid'>
+                        <span>救急</span>
+                    </div>
+                    <div class="item">
+                        <input type="checkbox" name="category" value='news'>
+                        <span>時事ニュース</span>
+                    </div>
+                    <div class="item">
+                        <input type="checkbox" name="category" value='various'>
+                        <span>その他</span>
+                    </div>
+                    <div class="item">
+                        <input type="checkbox" name="category" value='nocategory'>
+                        <span>指定なし</span>
+                    </div>
+                </div>
             </div>
-            <div class='seasons'>
-                <p>時期</p>
-                <input type="checkbox" name="category" value='spring'>
-                <span>春</span>
-                <input type="checkbox" name="category" value='summer'>
-                <span>夏</span>
-            </div>
-            <input type="submit" value="検索">
         </div>
-        <!--エラー時のエラーメッセージ出力-->
-        <! ?php include_once __DIR__ . '_errors.php' ?>
-    </form>
+        <div class="form_contents">
+            <div class="form_items">
+                <p>時期</p>
+                <div class="item_box">
+                    <div class="item">
+                        <input type="checkbox" name="category" value='spring'>
+                        <span>春</span>
+                    </div>
+                    <div class="item">
+                        <input type="checkbox" name="category" value='summer'>
+                        <span>夏</span>
+                    </div>
+                    <div class="item">
+                        <input type="checkbox" name="category" value='autumn'>
+                        <span>秋</span>
+                    </div>
+                    <div class="item">
+                        <input type="checkbox" name="category" value='winter'>
+                        <span>冬</span>
+                    </div>
+                    <div class="item">
+                        <input type="checkbox" name="category" value='noseason'>
+                        <span>指定なし</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="form_contents">
+            <button type="submit" class="search_btn form_btn">
+                <span>検索</span>
+                <i class="fa-sharp fa-solid fa-magnifying-glass"></i>
+            </button>
+        </div>
+    </section>
+    <!--エラー時のエラーメッセージ出力-->
+    <!-- ?php include_once __DIR__ . '_errors.php' ?>
+
 
 
 </body>
